f8e1ff48b53c543e1d542c40284b13ab88c733f8
teknikmekatronika <69633136+teknikmekatronika@users.noreply.github.com>
2020-08-13 21:00:50 +0700

    Add files via upload

---
+++ form2.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Amin Rusydi CV</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+    <link rel="stylesheet" type="text/css" href="assets/css/style.css">
+  </head>
+  <body>
+   <nav class="navbar fixed-top navbar-expand-lg navbar-dark page-navbar gradient">
+      <div class="container">
+        <a class="navbar-brand logo" href="#">Teknik Mekatronika</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+       
+          
+      </div>
+    </nav>
+    <main class="page cv-page">
+
+      <section class="cv-block info">
+        <div class="container">
+          <div class="work-experience group" id="work-experience">
+            <h4 class="text-center">FORM JAMINAN MUTU</h4>
+            <br>
+            <div class="item">
+              <div class="row">
+                <div class="col-md-6">
+                  <h3>Private tutor</h3>
+                  <h4 class="organization">My Home.</h4>
+                </div>
+               
+              </div>
+              <script type="text/javascript">var submitted=false;</script>
+    <iframe name="hidden_iframe" id="hidden_iframe" style="display:none;"     
+onload="if(submitted) {window.location='https://google.com';}"></iframe>
+      <form action="https://docs.google.com/forms/u/0/d/e/1FAIpQLSeOMTzxd-mdXQgW8VmIUmJJ_uiF5QobDzSD1jt5o_4Wuz8kVA/formResponse" method="post" target="hidden_iframe" 
+onsubmit="submitted=true;">
+
+            
+                <div class="form-group">
+                  <label for="exampleInputEmail1">Email address</label>
+                  <input type="email" class="form-control" name="emailAddress" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+                  <small id="emailHelp" class="form-text text-muted">Gunakan Email yang masih aktif.</small>
+                </div>
+                <div class="form-group">
+                  <label for="exampleInputPassword1">Nama Lengkap</label>
+                  <input type="text" class="form-control" name="entry.1608421867" id="exampleInputPassword1" placeholder="Nama Lengkap">
+                </div>
+                <div class="form-group">
+                  <label for="nim">NIM</label>
+                  <input type="text" class="form-control" name="entry.1562895314" id="nim" placeholder="Nomor Induk Mahasiswa">
+                </div>
+                <div class="form-group form-check">
+                  <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                  <label class="form-check-label" for="exampleCheck1">Check me out</label>
+                </div>
+                <button type="submit" class="btn btn-primary">Submit</button>
+              </form>
+            </div>
+      
+         
+      </section>
+      <footer class="page-footer">
+        <div class="container">
+          <div class="social-icons">
+            <a href="#"><i class="ion-social-facebook" title="Facebook"></i></a>
+            <a href="#"><i class="ion-social-instagram-outline" title="Instagram"></i></a>
+            <a href="#"><i class="ion-social-twitter" title="Twitter"></i></a>
+          </div>
+        </div>
+      </footer>
+    </main>
+  </body>
+  <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+  <script src="assets/js/script.js"></script>
+</html>
+
